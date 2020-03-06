cd14c18a384e4a499cda1dcd5889fb85d6dd0946
prhuft <gothrowayo@gmail.com>
2020-03-06 02:15:31 +0000

    SimplePage class

--- main.py
+++ main.py
@@ -14,51 +14,77 @@ import os
 from flask import Flask, request, jsonify
 import logging
 
-#### global vars
-version_idxs = ['A','B']
-version_idx = version_idxs[0]
-visit_dict = {key: 0 for key in version_idxs} # counts donate page visits
-home_visits = 0
+class SimplePage:
+    def __init__(self):
+        self.version_idxs = ['A','B']
+        self.version_idx = version_idxs[0]
+        self.visit_dict = {key: 0 for key in version_idxs} # counts donate page visits
+        self.home_visits = 0
+        self.app = Flask(__name__)
+    
+    def donate_count(self, page_fn):
+        def wrapper(self, page):
+            if page == 'donate.html':
+                self.visit_dict[version_idx] += 1
+            return page_fn
+        return wrapper
 
-def donate_count(page_fn):
-    def wrapper(page):
-        if page == 'donate.html':
-            visit_dict[version_idx] += 1
-        return page_fn
-    return wrapper
-
-def home_version(home_fn):
-    visit_dict = {key: 0 for key in version_idxs}    
-    def wrapper():
-        pass
+    def home_version(self, home_fn):
+        self.visit_dict = {key: 0 for key in version_idxs}    
+        def wrapper():
+            pass
+        
+    @app.route('/')
+    def home(self): 
+        global version_idx, home_visits
+        if home_visits <= 10:
+            if home_visits == 10:
+                version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 5-int(x[1]))[0][0]
+            version_idx = version_idxs[home_visits % 2]
+            home_visits += 1
+        with open("index"+ version_idx +".html") as f:
+            html = f.read()
+        return html
+    
+    @donate_count
+    @app.route('/<html_page>')
+    def page(self, html_page):
+        if html_page == 'browse.html':
+            with open('main.csv') as f:
+                df = pd.read_csv(f)
+            return df[-1000:].to_html() 
+        with open(html_page) as f:
+            html = f.read()
+                # if there are other resources for this page, load those too
+        return html
+    
         
 #### set up a flask app
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def home(): # switches btwn A,B until 10 visits. DOESN'T UPDATE global home_visits. use a wrapper to do the counting and store the final idx result, so that idx will simply be returned after this. OR. 
-# make this webpage inside a class, then home_visits is a property we can set update inside the function. 
-    global version_idx, home_visits
-    if home_visits <= 10:
-        if home_visits == 10:
-            version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 5-int(x[1]))[0][0]
-        version_idx = version_idxs[home_visits % 2]
-        home_visits += 1
-    with open("index"+ version_idx +".html") as f:
-        html = f.read()
-    return html
+# @app.route('/')
+# def home(): 
+#     global version_idx, home_visits
+#     if home_visits <= 10:
+#         if home_visits == 10:
+#             version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 5-int(x[1]))[0][0]
+#         version_idx = version_idxs[home_visits % 2]
+#         home_visits += 1
+#     with open("index"+ version_idx +".html") as f:
+#         html = f.read()
+#     return html
 
-@donate_count
-@app.route('/<html_page>')
-def page(html_page):
-    if html_page == 'browse.html':
-        with open('main.csv') as f:
-            df = pd.read_csv(f)
-        return df[-1000:].to_html() 
-    with open(html_page) as f:
-        html = f.read()
-            # if there are other resources for this page, load those too
-    return html
+# @donate_count
+# @app.route('/<html_page>')
+# def page(html_page):
+#     if html_page == 'browse.html':
+#         with open('main.csv') as f:
+#             df = pd.read_csv(f)
+#         return df[-1000:].to_html() 
+#     with open(html_page) as f:
+#         html = f.read()
+#             # if there are other resources for this page, load those too
+#     return html
 
 #### generate the index.html files
 files = [f for f in os.listdir('.') if 
@@ -76,6 +102,6 @@ for idx in version_idxs:
         f.write(text)
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-
+    new_page = SimplePage()
+    new_page.app.run(host="0.0.0.0")
 
