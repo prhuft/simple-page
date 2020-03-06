126356bf14edd5e08dc1db2d092f08e66b4a6f59
prhuft <gothrowayo@gmail.com>
2020-03-06 02:52:46 +0000

    query strings working

--- main.py
+++ main.py
@@ -20,12 +20,14 @@ version_idx = version_idxs[0]
 visit_dict = {key: 0 for key in version_idxs} # counts donate page visits
 home_visits = 0
 
-def donate_count(page_fn):
-    def wrapper(page):
-        if page == 'donate.html':
-            visit_dict[version_idx] += 1
-        return page_fn
-    return wrapper
+# def donate_count(page_fn):
+#     visit_dict = {key: 0 for key in version_idxs} # counts donate page visits
+#     home_visits = 0
+#     def wrapper(page):
+#         if page == 'donate.html':
+#             visit_dict[version_idx] += 1
+#         return page_fn
+#     return wrapper
 
 def home_version(home_fn):
     visit_dict = {key: 0 for key in version_idxs}    
@@ -38,7 +40,8 @@ app = Flask(__name__)
 @app.route('/')
 def home(): # switches btwn A,B until 10 visits. DOESN'T UPDATE global home_visits. use a wrapper to do the counting and store the final idx result, so that idx will simply be returned after this. OR. 
 # make this webpage inside a class, then home_visits is a property we can set update inside the function. 
-    global version_idx, home_visits
+    global version_idx
+    global home_visits
     if home_visits <= 10:
         if home_visits == 10:
             version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 5-int(x[1]))[0][0]
@@ -48,16 +51,24 @@ def home(): # switches btwn A,B until 10 visits. DOESN'T UPDATE global home_visi
         html = f.read()
     return html
 
-@donate_count
-@app.route('/<html_page>')
-def page(html_page):
-    if html_page == 'browse.html':
-        with open('main.csv') as f:
-            df = pd.read_csv(f)
-        return df[-1000:].to_html() 
-    with open(html_page) as f:
+# @donate_count
+# @app.route('/<html_page>')
+# def page(html_page):
+#     if html_page == 'browse.html':
+#         with open('main.csv') as f:
+#             df = pd.read_csv(f)
+#         return df[-1000:].to_html() 
+#     with open(html_page) as f:
+#         html = f.read()
+#             # if there are other resources for this page, load those too
+#     return html
+
+@app.route('/donate.html')
+def donate_page():
+    version = request.args.get('from')
+    visit_dict[version] += 1
+    with open('donate.html') as f:
         html = f.read()
-            # if there are other resources for this page, load those too
     return html
 
 #### generate the index.html files
@@ -71,7 +82,7 @@ for idx in version_idxs:
         <body><p>Directory</p><ul> """
         for file in files:
             if 'index' not in file:
-                text += "<li><a href=" + '/' + file + ">" + file[:-5] + "</a></li>"           
+                text += "<li><a href=" + '/' + file + f'?from={idx}' ">" + file[:-5] + "</a></li>"           
         text += "</ul> \n </body> \n </html>"
         f.write(text)
     
