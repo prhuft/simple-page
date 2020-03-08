b1b4849b8b08de57014596af68e6f4b1ced08b7a
prhuft <gothrowayo@gmail.com>
2020-03-08 02:40:55 +0000

    format in generating index.html

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import logging
 
 #### global vars
 version_idxs = ['A','B']
-version_idx = None #version_idxs[0]
+version_idx = 'A'
 visit_dict = {key: 0 for key in version_idxs} # counts donate page visits
 home_visits = 0
         
@@ -24,48 +24,54 @@ home_visits = 0
 app = Flask(__name__)
 
 @app.route('/')
-def home():  
-    global version_idx
-    global home_visits
+def home_page():  
+    global version_idx, home_visits
     if home_visits <= 10:
         version_idx = version_idxs[home_visits % 2]
         if home_visits == 10:
-            version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 5-int(x[1]))[0][0]
+            version_idx = sorted([(idx,visit_dict[idx]) for idx in visit_dict],key=lambda x: 10-int(x[1]))[0][0]
         home_visits += 1
-    app.logger.critical(f'home_visits = {home_visits}')
-    app.logger.critical(f'visit_dict = {visit_dict}')
-    app.logger.critical(f'version_idx = {version_idx}')
-
     with open("index"+ version_idx +".html") as f:
         html = f.read()
     return html
 
+@app.route('/api.html')
+def api_page():
+    with open('api.html') as f:
+        return f.read()
+
 @app.route('/donate.html')
 def donate_page():
     version = request.args.get('from')
-    visit_dict[version] += 1
-    app.logger.critical(f'home_visits = {home_visits}')
-    app.logger.critical(f'visit_dict = {visit_dict}')
-    app.logger.critical(f'version_idx = {version_idx}')
+    if version in visit_dict:
+        visit_dict[version] += 1
     with open('donate.html') as f:
-        html = f.read()
-    return html
+        return f.read()
 
+@app.route('/browse.html')
+def browse_page():
+    # get arguments like year, *other paramters*
+    # pass these args as items in a list to index the dataframe by column
+    with open('main.csv') as f: # build the df globally to avoid rebuilding
+        data_html = pd.read_csv(f).to_html() #TODO: reset index
+    return "<h1>Checkout this hot data (hehe.. get it? because global warming)!</h1>" + data_html
+    
 #### generate the index.html files
 files = [f for f in os.listdir('.') if 
-         (os.path.isfile(f) and f[-4:]=='html')]
+         (os.path.isfile(f) and f[-4:]=='html' and 'index' not in f)]
 for idx in version_idxs:
     with open("index"+ idx +".html", 'w') as f:
         text = f"""<html>
-        <h1>Welcome to my webpage (version={idx})</h1>
-        <body><p>Directory</p><ul> """
+             <body>   
+             <h1>Welcome to my webpage (version={idx})</h1>
+             <p>Directory <br /> \n"""
         for file in files:
-            if 'index' not in file:
-                text += "<li><a href=" + '/' + file + f'?from={idx}' ">" + file[:-5] + "</a></li>"           
-        text += "</ul> \n </body> \n </html>"
+            if 'donate' in file:
+                text += f"<a href = '{file}?from={idx}'>{file[:-5]}</a><br /> \n"
+            else: # could use a regex insertion here instead
+                text += f"<a href = '{file}'>{file}</a><br /> \n"     
+        text += "</p> \t \n</body>\n</html>"
         f.write(text)
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
-
-
