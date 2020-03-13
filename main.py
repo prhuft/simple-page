a164a22b296fe579f9b993f5c01a6b4b4b25d54e
prhuft <gothrowayo@gmail.com>
2020-03-13 03:50:22 +0000

    subscription interface

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ About the data:
 import pandas as pd
 import os
 from flask import Flask, request, jsonify
+import re
 import logging
 
 #### global vars
@@ -65,8 +66,8 @@ def browse_page(ext='html'):
         row1,row2 = [int(x) for x in qdict['rows'].split('-')]
         df = df.iloc[row1:row2]
     #TODO: make filterable by column value
-#     for key in [key for key in qdict if key in df.keys() and qdict[key] is not None]:
-#         df = df[df[key] == qdict[key]]
+    for key in [key for key in qdict if key in df.keys() and qdict[key] is not None]:
+        df = df[df[key] == qdict[key]]
     if ext == 'json':
         rows = [row[1].to_dict() for row in df.iterrows()]
         if not len(rows) - 1:
@@ -78,11 +79,12 @@ def browse_page(ext='html'):
 @app.route('/email', methods=["POST"])
 def email():
     email = str(request.data, "utf-8")
-    if re.match(r"????", email):
+    if re.match(r"[a-zA-Z0-9]+\.*[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+",
+                email):
         with open("emails.txt", "a") as f: # open file in append mode
-            ????
+            f.write(email + '\n')
         return jsonify("thanks")
-    return jsonify(????)
+    return jsonify("please carefully re-enter your email, this time without typos.")
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
